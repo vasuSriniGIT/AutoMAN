f0dd8551229feb2ed5ce8a976284fc69ddb18316
= <=>
2025-02-20 20:58:34 +0100

    Devkitexplore added

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from truckscenes import TruckScenes
 
-# Provide the path to the root directory of the dataset
-dataset_root = r'D:\Srini\Autonomous_Proj\Data_MAN\man-truckscenes_metadata_v1.0-mini\man-truckscenes'
+# Path for initializing TruckScenes
+metadata_path = 'D:/Srini/Autonomous_Proj/Data_MAN/man-truckscenes_metadata_v1.0-mini/man-truckscenes'
 
 # Initialize the TruckScenes object
-trucksc = TruckScenes('v1.0-mini', dataset_root, True)
-
-# List available scenes
-trucksc.list_scenes()
+trucksc = TruckScenes('v1.0-mini', metadata_path, True)
 
 # Select the first scene
 my_scene = trucksc.scene[0]
@@ -17,3 +14,6 @@ first_sample_token = my_scene['first_sample_token']
 
 # Render the first sample
 trucksc.render_sample(first_sample_token)
+
+my_sample = trucksc.get('sample', first_sample_token)
+my_sample
